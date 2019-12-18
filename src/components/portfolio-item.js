4bf0b8686de79e1be342590727ae6c3a2f407e4a
dallinbledsoe <dallinbledsoe@gmail.com>
2019-12-18 15:15:29 -0700

    added props for titles and urls

--- src/components/portfolio-item.js
+++ src/components/portfolio-item.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-export default function() {
+export default function(props) {
     return (
         <div>
             <h3>
-                Portfolio item
-            </h3>
+                {props.title}
+                </h3>
+               <h4> {props.url}
+            </h4>
+
         </div>
     )
 }
