97b39364d28ef65442d56e0cfeeb6942f5d90080
dallinbledsoe <dallinbledsoe@gmail.com>
2019-12-18 13:54:12 -0700

    added portfolio container component

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import PortfolioContainer from './portfolio-container';
 
 
 export default class App extends Component {
@@ -8,6 +9,7 @@ export default class App extends Component {
       <div className='app'>
         <h1>Dallin Bledsoe</h1>
         <h2>there ya go</h2>
+        <PortfolioContainer />
         <div>
         {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import moment from "moment";
+
+export default class PortfolioContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Portfolio items go here updated...</h2>
+                </div>
+        
+        )
+    }
+
+}
