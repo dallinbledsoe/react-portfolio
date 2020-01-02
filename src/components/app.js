8abd4add73862fde5e2302c405290bc4147bf6f7
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-02 16:08:08 -0700

    added nav component w a conditional btn

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import moment from "moment";
 import PortfolioContainer from './portfolio-container';
+import NavigationContainer from "./navigation/navigation-container"
 
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>Dallin Bledsoe</h1>
         <div>
         {moment().format('MMMM Do YYYY, h:mm:ss a')}
