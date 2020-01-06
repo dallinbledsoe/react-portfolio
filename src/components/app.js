756d8e417f6cc55c485a6cc40c1a3c86dc6a16f9
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-06 16:01:09 -0700

    added link on blog page and screwed something up

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PortfolioContainer from './portfolio-container';
+
 import NavigationContainer from "./navigation/navigation-container";
-import Home from "./pages/home";
-import About from "./pages/about";
+import Home from "./pages/home"
+import About from "./pages/about"
 import Blog from "./pages/blog";
 import Contact from "./pages/contact"
 
@@ -14,24 +14,25 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
+          <h1>Dallin Bledsoe</h1>
+        <div>
+        {moment().format('MMMM Do YYYY, h:mm:ss a')}
+
+        </div>
         <NavigationContainer />
-        <router>
+        <Router>
           <div>
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/about-me" component={About} />
-              {/* <Route path="/blog" component={Blog} />
-              <Route path="/contact" component={Contact} /> */}
+              <Route path="/blog" component={Blog} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/portfolio:slug" component={PortfolioDetail} />
             </Switch>
           </div>
-        </router>
+        </Router>
 
-
-        <h1>Dallin Bledsoe</h1>
-        <div>
-        {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        <PortfolioContainer />
-        </div>
+      
       </div>
     );
   }
