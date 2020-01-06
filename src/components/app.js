cad8cb2b72b18e32130098aaeac91a51e9f464a8
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-06 15:22:49 -0700

    added links home and about

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PortfolioContainer from './portfolio-container';
-import NavigationContainer from "./navigation/navigation-container"
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Blog from "./pages/blog";
+import Contact from "./pages.contact"
+
 
 
 export default class App extends Component {
@@ -9,6 +15,16 @@ export default class App extends Component {
     return (
       <div className='app'>
         <NavigationContainer />
+        <router>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+            </Switch>
+          </div>
+        </router>
+
+
         <h1>Dallin Bledsoe</h1>
         <div>
         {moment().format('MMMM Do YYYY, h:mm:ss a')}
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,5 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
+import { NavLink } from 'react-router-dom'
 
 export default class NavigationContainer extends Component {
     constructor(){
@@ -7,8 +8,8 @@ export default class NavigationContainer extends Component {
     render() {
         return(
             <div> 
-                <button>Home</button> 
-                <button>About</button> 
+                <NavLink exact to="/">Home</NavLink>
+                <NavLink to="/about-me">About</NavLink>
                 <button>Contact</button> 
                 <button>Blog</button> 
                 {false ? <button>Add Blog</button> : '' }
