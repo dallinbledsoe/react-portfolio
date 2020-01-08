72f3de812716fd227a000d86d9f47781d25d7e56
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-08 15:47:08 -0700

    added authentication page

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Auth from "./pages/auth"
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
