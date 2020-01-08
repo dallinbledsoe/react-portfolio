edcd9dd85244ffdb3ca51b0cc186691c09cc4ec7
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-08 16:07:07 -0700

    put image in auth

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import loginImg from "../../../static/assets/images/auth/login.jpg"
 
 export default class Auth extends Component {
     render() {
         return (
-            <div>
-                Auth...
+            <div className="auth-page-wrapper">
+                <div className="left-column"
+                    style={{
+                        backgroundImage: `url(${loginImg})`
+                    }}
+                    >
+                </div>
+                <div className="right-column">
+                    <h1>login here</h1>
+                </div>
             </div>
         )
     }
