e6130728597c4c500ed097fb95ab2becda777d1a
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-08 13:45:24 -0700

    added grid columns to portfolio items

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -72,7 +72,8 @@ export default class PortfolioContainer extends Component {
                 <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
                 <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
                 <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                {this.portfolioItems()}
+               
+               <div className="portfolio-items-wrapper"> {this.portfolioItems()} </div>
                 </div>
         
         )
