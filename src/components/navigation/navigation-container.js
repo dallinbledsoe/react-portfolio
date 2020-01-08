954691420acceb505401878db4af87b52b9f9174
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-07 17:27:46 -0700

    styled nav-bar

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -10,19 +10,26 @@ export default class NavigationComponent extends Component {
     return (
       <div className="nav-wrapper">
         <div className="left-side">
+          <div className="nav-link-wrapper">
         <NavLink exact to="/" activeClassName="nav-link-active">
           Home
         </NavLink>
-
+        </div>
+        <div className="nav-link-wrapper">
         <NavLink to="/about-me" activeClassName="nav-link-active">
           About
         </NavLink>
+        </div>
+        <div className="nav-link-wrapper">
         <NavLink to="/contact" activeClassName="nav-link-active">
           Contact
         </NavLink>
+        </div>
+        <div className="nav-link-wrapper">
         <NavLink to="/blog" activeClassName="nav-link-active">
           Blog
         </NavLink>
+        </div>
 
         {false ? <button>Add Blog</button> : null}
         </div>
