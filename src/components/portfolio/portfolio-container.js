e09a50eeace5579ad21e2fceb64f1cb9dbaf76ed
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-08 15:26:42 -0700

    styled buttons

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -67,13 +67,12 @@ export default class PortfolioContainer extends Component {
         }
 
         return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+            <div className="portfolio-items-wrapper">
+                <button className="btn" onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+                <button className="btn"  onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+                <button className="btn"  onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
                
-               <div className="portfolio-items-wrapper"> {this.portfolioItems()} </div>
+               {this.portfolioItems()}
                 </div>
         
         )
