161024060612d0181097a55a883ff69861ea6ac0
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-07 17:12:12 -0700

    added flexbox to nav-bar

--- src/components/app.js
+++ src/components/app.js
@@ -17,11 +17,6 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-          <h1>Dallin Bledsoe</h1>
-        <div>
-        {moment().format('MMMM Do YYYY, h:mm:ss a')}
-
-        </div>
         <Router>
           <div>
         <NavigationContainer />
