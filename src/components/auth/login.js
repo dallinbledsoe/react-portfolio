9ffd8d435b7561b02785ae5ff4ea62d0dd7213c7
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-16 17:12:06 -0700

    finished styling form

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -80,7 +80,7 @@ export default class Login extends Component {
           />
 
           <div>
-            <button type="submit">Login</button>
+            <button className="btn" type="submit">Login</button>
           </div>
         </form>
       </div>
