f06f77662c73eb0ac042649ad8c04f3ec5f3b1cd
dallinbledsoe <dallinbledsoe@gmail.com>
2020-01-18 09:26:43 -0700

    remove image on edit mode

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -30,10 +30,15 @@ export default class PortfolioForm extends Component {
     this.handleThumbDrop = this.handleThumbDrop.bind(this);
     this.handleBannerDrop = this.handleBannerDrop.bind(this);
     this.handleLogoDrop = this.handleLogoDrop.bind(this);
+    this.deleteImage = this.deleteImage.bind(this);
 
     this.thumbRef = React.createRef();
     this.bannerRef = React.createRef();
     this.logoRef = React.createRef();
+  }
+
+  deleteImage() {
+    console.log("delete image")
   }
 
   componentDidUpdate() {
@@ -229,7 +234,11 @@ export default class PortfolioForm extends Component {
 
           {this.state.thumb_image && this.state.editMode ? (
             <div className="portfolio-manager-image-wrapper">
-          <img src={this.state.thumb_image} /> </div> )
+          <img src={this.state.thumb_image} /> 
+          <div className="image-removal-link">
+            <a onClick={() => this.deleteImage("thumb_image")}>Remove File</a>
+            </div>
+            </div> )
           : (
         
             <DropzoneComponent
@@ -246,7 +255,10 @@ export default class PortfolioForm extends Component {
 
           {this.state.banner_image && this.state.editMode ? (
             <div className="portfolio-manager-image-wrapper">
-          <img src={this.state.banner_image} /> </div> )
+          <img src={this.state.banner_image} />
+          <div className="image-removal-link">
+            <a onClick={() => this.deleteImage("thumb_image")}>Remove File</a>
+            </div> </div> )
           : (
           
             <DropzoneComponent
@@ -259,7 +271,10 @@ export default class PortfolioForm extends Component {
             )}
           {this.state.logo && this.state.editMode ? (
             <div className="portfolio-manager-image-wrapper">
-          <img src={this.state.logo} /> </div> )
+          <img src={this.state.logo} /> 
+          <div className="image-removal-link">
+            <a onClick={() => this.deleteImage("thumb_image")}>Remove File</a>
+            </div> </div> )
           : (
             <DropzoneComponent
               ref={this.logoRef}
