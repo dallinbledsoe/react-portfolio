1ef98aefa743d735036972077b94843ab7ebba17
dallinbledsoe <dallinbledsoe@gmail.com>
2020-02-10 16:10:26 -0700

    changed categories

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -78,9 +78,9 @@ export default class PortfolioContainer extends Component {
         return (
             <div className="homepage-wrapper">
                 <div className="filter-links">                
-                <button className="btn" onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button className="btn"  onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-                <button className="btn"  onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                {/* <button className="btn" onClick={() => this.handleFilter('Scheduling')}></button> */}
+                <button className="btn"  onClick={() => this.handleFilter('Enterprise')}>HTML/CSS</button>
+                <button className="btn"  onClick={() => this.handleFilter('eCommerce')}>React.js</button>
                 <button className="btn"  onClick={() => this.handleFilter('CLEAR_FILTERS')}>All</button>
                </div>
             <div className="portfolio-items-wrapper">
